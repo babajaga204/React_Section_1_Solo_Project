babe3f18446b0bd0afedd934222ab49bfbab9f48
Simen Kristiansen <simenbolstad@gmail.com>
2022-11-10 15:43:42 +0100

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Info() {
   return (
     <div className="info--container">
-      <img src="./public/simen.jpg" />
+      <img src="/simen.jpg" />
       <h1>Simen Kristiansen</h1>
       <h5>Frontend Developer</h5>
       <button>Email</button>
